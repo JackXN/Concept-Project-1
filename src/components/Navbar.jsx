2288fdaf967d56d1bd2dc449a23ff840af792b6d
Jack Rigan <riganjack40@gmail.com>
2021-04-07 15:02:30 -0700

    Updated Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,83 @@
+import React, {useState} from 'react';
+import {Link, BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {MdFingerprint} from 'react-icons/md'
+import {FaBars, FaTimes } from 'react-icons/fa'
+import Button from './Button';
+import "./Navbar.css"
+
+
+
+const Navbar = () => {
+
+    const [click, setClick] = useState(false);
+const [button, setButton] = useState(true)
+
+
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+const showButton = () => {
+    if(window.innerWidth <= 960) {
+        setButton(false)
+    }else {
+        setButton(true)
+    }
+}
+        window.addEventListener('resize', showButton)
+
+    return (
+        <>
+        <Router>
+        <div className="navbar">
+        <div className="navbar-container container">
+
+            <Link className = 'navbar-logo' to ='/'>
+                <MdFingerprint className= "navbar-icons"/>
+                Lavish
+            </Link>
+
+            <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes/> : <FaBars/>}
+            </div>
+        <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
+
+        <li className="nav-item">
+            <Link to = '/'className = 'nav-links'>
+                Home
+            </Link>
+        </li>
+        <li className="nav-item">
+            <Link to = '/services'className = 'nav-links'>
+                Services
+            </Link>
+        </li>
+        <li className="nav-item">
+            <Link to = '/products'className = 'nav-links'>
+                Products
+            </Link>
+        </li>
+
+       <li className="nav-btn">
+           {button ? (
+               <Link to='/sign-up' className = 'btn-link'>
+                   <Button buttonStyle='btn-outline'>Sign Up</Button>
+               </Link>
+           ): (
+               <Link to= '/sign-up' className = 'btn-link'>
+                   <Button buttonStyle = 'btn--outline'
+                   buttonSize='btn--mobile'>Sign Up</Button>
+               </Link>
+           )}
+       </li>
+
+
+        </ul>
+        </div>
+        </div>
+        </Router>
+        </>
+    )
+}
+
+export default Navbar
