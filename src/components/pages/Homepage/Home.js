20b529ed0732716afdfab604acf41b66f62bfaab
Jack Rigan <riganjack40@gmail.com>
2021-04-07 20:12:34 -0700

    updated homeObj in Data.js

--- src/components/pages/Homepage/Home.js
+++ src/components/pages/Homepage/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import HeroSection from '../../HeroSection'
-import {homeObjOne} from './Data'
+import {homeObjOne, homeObjFour, homeObjThree,homeObjTwo} from './Data'
 
 const Home = () => {
 
 return (
 <>
 <HeroSection {...homeObjOne}/>
+<HeroSection {...homeObjTwo}/>
+<HeroSection {...homeObjThree}/>
+<HeroSection {...homeObjFour}/>
+
 
 </>
 )
